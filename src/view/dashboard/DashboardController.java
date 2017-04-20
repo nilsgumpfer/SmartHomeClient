fd4244d2b0bc73b0f2ea4fd38e802e23c1c1625a
Nils Gumpfer <nilsgumpfer94@arcor.de>
2017-04-20 22:54:53 +0200

    GUI: fx:IDs assigned

--- src/view/dashboard/DashboardController.java
+++ src/view/dashboard/DashboardController.java
@@ -1,4 +1,369 @@
 package view.dashboard;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class DashboardController {
+
+    @FXML
+    private ImageView thermometer_AddDeviceButton;
+
+    @FXML
+    private Label shutter_Model2;
+
+    @FXML
+    private Label thermometer_Serialnumber;
+
+    @FXML
+    private Label shutter_Model1;
+
+    @FXML
+    private ImageView shutter_MoveDown1;
+
+    @FXML
+    private ImageView shutter_MoveDown2;
+
+    @FXML
+    private Label shutter_Model4;
+
+    @FXML
+    private ImageView shutter_MoveDown3;
+
+    @FXML
+    private Label shutter_Model3;
+
+    @FXML
+    private ImageView shutter_MoveDown4;
+
+    @FXML
+    private Label thermometer_Name;
+
+    @FXML
+    private Label weatherstation_Model;
+
+    @FXML
+    private Label heating_Manufacturer;
+
+    @FXML
+    private Label shutter_Serialnumber3;
+
+    @FXML
+    private Label shutter_Serialnumber4;
+
+    @FXML
+    private Label shutter_Serialnumber1;
+
+    @FXML
+    private Label shutter_Serialnumber2;
+
+    @FXML
+    private ImageView connectionIndicatorImage;
+
+    @FXML
+    private ImageView shutter_StepUp3;
+
+    @FXML
+    private ImageView shutter_StepUp4;
+
+    @FXML
+    private ImageView shutter_StepUp1;
+
+    @FXML
+    private ImageView shutter_StepUp2;
+
+    @FXML
+    private ImageView weatherstation_EditDeviceButton;
+
+    @FXML
+    private ImageView weatherstation_DeleteDeviceButton;
+
+    @FXML
+    private ImageView shutter_AddDeviceButton;
+
+    @FXML
+    private Label heating_Serialnumber;
+
+    @FXML
+    private Label heating_PowerState;
+
+    @FXML
+    private ImageView weatherstation_DeviceImage;
+
+    @FXML
+    private Label weatherstation_Serialnumber;
+
+    @FXML
+    private Label shutter_Position3;
+
+    @FXML
+    private Label weatherstation_Temperature;
+
+    @FXML
+    private Label shutter_Position4;
+
+    @FXML
+    private Label weatherstation_Pressure;
+
+    @FXML
+    private Label shutter_Manufacturer3;
+
+    @FXML
+    private Label shutter_Manufacturer4;
+
+    @FXML
+    private Label shutter_Position1;
+
+    @FXML
+    private Label shutter_Position2;
+
+    @FXML
+    private ImageView heating_AddDeviceButton;
+
+    @FXML
+    private Label thermometer_Manufacturer;
+
+    @FXML
+    private Label shutter_Manufacturer1;
+
+    @FXML
+    private Label shutter_Manufacturer2;
+
+    @FXML
+    private ImageView shutter_StepDow3;
+
+    @FXML
+    private ImageView shutter_StepDow4;
+
+    @FXML
+    private ImageView shutter_EditDeviceButton;
+
+    @FXML
+    private Label heating_Model;
+
+    @FXML
+    private ImageView undoButton;
+
+    @FXML
+    private ImageView thermometer_DeleteDeviceButton;
+
+    @FXML
+    private Label weatherstation_WindVelocity;
+
+    @FXML
+    private ImageView heating_PowerButton;
+
+    @FXML
+    private Label heating_Temperature;
+
+    @FXML
+    private ImageView shutter_DeviceImage;
+
+    @FXML
+    private ImageView shutter_CollectiveDownButton;
+
+    @FXML
+    private Label thermometer_Model;
+
+    @FXML
+    private Label heating_Mode;
+
+    @FXML
+    private Label thermometer_Temperature;
+
+    @FXML
+    private ImageView heating_DeviceImage;
+
+    @FXML
+    private Label shutter_Name1;
+
+    @FXML
+    private Label shutter_Name2;
+
+    @FXML
+    private Label shutter_Name3;
+
+    @FXML
+    private ImageView shutter_CollectiveUpButton;
+
+    @FXML
+    private Label shutter_Name4;
+
+    @FXML
+    private ImageView shutter_DeleteDeviceButton;
+
+    @FXML
+    private ImageView thermometer_EditDeviceButton;
+
+    @FXML
+    private ImageView heating_DeleteDeviceButton;
+
+    @FXML
+    private Label weatherstation_Rainfall;
+
+    @FXML
+    private ImageView shutter_MoveUp4;
+
+    @FXML
+    private ImageView shutter_MoveUp3;
+
+    @FXML
+    private ImageView shutter_MoveUp2;
+
+    @FXML
+    private ImageView shutter_MoveUp1;
+
+    @FXML
+    private ImageView shutter_StepDown1;
+
+    @FXML
+    private ImageView shutter_StepDown2;
+
+    @FXML
+    private Label weatherstation_Name;
+
+    @FXML
+    private ImageView weatherstation_AddDeviceButton;
+
+    @FXML
+    private Label weatherstation_Manufacturer;
+
+    @FXML
+    private ImageView heating_EditDeviceButton;
+
+    @FXML
+    private ImageView thermometer_DeviceImage;
+
+    @FXML
+    private Label heating_Name;
+
+    public void initController(){
+
+    }
+
+    public void heating_PowerButton_mouseReleased(){
+
+    }
+
+    public void heating_AddDeviceButton_mouseReleased(){
+
+    }
+
+    public void heating_EditDeviceButton_mouseReleased(){
+
+    }
+
+    public void heating_DeleteDeviceButton_mouseReleased(){
+
+    }
+
+    public void shutter_CollectiveDownButton_mouseReleased(){
+
+    }
+
+    public void shutter_CollectiveUpButton_mouseReleased(){
+
+    }
+
+    public void shutter_AddDeviceButton_mouseReleased(){
+
+    }
+
+    public void shutter_EditDeviceButton_mouseReleased(){
+
+    }
+
+    public void shutter_DeleteDeviceButton_mouseReleased(){
+
+    }
+
+    public void shutter_StepUp1_mouseReleased(){
+
+    }
+
+    public void shutter_StepUp2_mouseReleased(){
+
+    }
+
+    public void shutter_StepUp3_mouseReleased(){
+
+    }
+
+    public void shutter_StepUp4_mouseReleased(){
+
+    }
+
+    public void shutter_StepDown1_mouseReleased(){
+
+    }
+
+    public void shutter_StepDown2_mouseReleased(){
+
+    }
+    public void shutter_StepDown3_mouseReleased(){
+
+    }
+
+    public void shutter_StepDown4_mouseReleased(){
+
+    }
+
+    public void shutter_MoveUp1_mouseReleased(){
+
+    }
+
+    public void shutter_MoveUp2_mouseReleased(){
+
+    }
+
+    public void shutter_MoveUp3_mouseReleased(){
+
+    }
+
+    public void shutter_MoveUp4_mouseReleased(){
+
+    }
+
+    public void shutter_MoveDown1_mouseReleased(){
+
+    }
+
+    public void shutter_MoveDown2_mouseReleased(){
+
+    }
+    public void shutter_MoveDown3_mouseReleased(){
+
+    }
+
+    public void shutter_MoveDown4_mouseReleased(){
+
+    }
+
+    public void weatherstation_AddDeviceButton_mouseReleased(){
+
+    }
+
+    public void weatherstation_EditDeviceButton_mouseReleased(){
+
+    }
+
+    public void weatherstation_DeleteDeviceButton_mouseReleased(){
+
+    }
+
+    public void thermometer_AddDeviceButton_mouseReleased(){
+
+    }
+
+    public void thermometer_EditDeviceButton_mouseReleased(){
+
+    }
+
+    public void thermometer_DeleteDeviceButton_mouseReleased(){
+
+    }
+
+    public void undoButton_mouseReleased(){
+
+    }
 }
